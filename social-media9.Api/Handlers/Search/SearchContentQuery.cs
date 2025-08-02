5f6150642246c18ffe61d9cb2e4f6e266dcb81c7
Kgothatso Moshoeshoe <Kgothatso.Moshoeshoe@bbd.co.za>
2025-08-02 09:52:35 +0200

    search content

---
+++ social-media9.Api/Handlers/Search/SearchContentQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using social_media9.Api.Data;
+using social_media9.Api.Models;
+
+namespace social_media9.Api.Handlers.Search;
+
+// Using a record for a simple, immutable query object
+public record SearchContentQuery(string Query, int Limit) : IRequest<IEnumerable<VideoPostSummary>>;
+
+public class SearchContentQueryHandler : IRequestHandler<SearchContentQuery, IEnumerable<VideoPostSummary>>
+{
+  private readonly ISearchRepository _repository;
+
+  public SearchContentQueryHandler(ISearchRepository repository)
+  {
+    _repository = repository;
+  }
+
+  public Task<IEnumerable<VideoPostSummary>> Handle(SearchContentQuery request, CancellationToken cancellationToken)
+  {
+    return _repository.SearchContentAsync(request.Query, request.Limit, cancellationToken);
+  }
+}
