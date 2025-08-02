3f948d0efb96c0409eb115101cf65ab903296eed
Kgothatso Moshoeshoe <Kgothatso.Moshoeshoe@bbd.co.za>
2025-08-02 09:53:10 +0200

    user summary

---
+++ social-media9.Api/Models/UserSummary.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace social_media9.Api.Models;
+
+public class UserSummary
+{
+  [JsonPropertyName("userId")]
+  public string UserId { get; set; }
+
+  [JsonPropertyName("username")]
+  public string Username { get; set; }
+
+  [JsonPropertyName("fullName")] // Important for better search quality
+  public string FullName { get; set; }
+
+  [JsonPropertyName("profilePictureUrl")]
+  public string ProfilePictureUrl { get; set; }
+}
