f94ec94e55db6fdd0fc7594258498393c7e17d3c
Sam <samkele@bbd.co.za>
2025-08-03 21:03:53 +0200

    added inbound and outbound queues

--- terraform/messaging.tf
+++ terraform/messaging.tf
@@ -1,3 +1,7 @@
-resource "aws_sqs_queue" "main" {
-  name = "${var.project_name}-main-queue"
+resource "aws_sqs_queue" "inbound_queue" {
+  name = "${var.project_name}-inbound-queue"
+}
+
+resource "aws_sqs_queue" "outbound_queue" {
+  name = "${var.project_name}-outbound-queue"
 }
