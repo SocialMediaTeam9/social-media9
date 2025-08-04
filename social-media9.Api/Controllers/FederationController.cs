dfbcf356ea522535cd37b1c3bdd5418614f61dca
Sam <samkele@bbd.co.za>
2025-08-04 19:55:20 +0200

    updated federation controller

--- social-media9.Api/Controllers/FederationController.cs
+++ social-media9.Api/Controllers/FederationController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using social_media9.Api.Models;
+using social_media9.Api.Services.DynamoDB;
 
 namespace social_media9.Api.Controllers;
 
@@ -9,59 +10,56 @@ namespace social_media9.Api.Controllers;
 [Authorize(Policy = "InternalApi")]
 public class FederationController : ControllerBase
 {
-    // private readonly DynamoDbService _dbService;
+    private readonly DynamoDbService _dbService;
 
-    // public FederationController(DynamoDbService dbService)
-    // {
-    //     _dbService = dbService;
-    // }
-
-
-    public FederationController()
+    public FederationController(DynamoDbService dbService)
     {
+        _dbService = dbService;
     }
+
+
 
     [HttpPost("user")]
     public async Task<IActionResult> GetUserInfo([FromBody] GtsUserInfoRequest request)
     {
-        // var userEntity = await _dbService.GetUserProfileByUsernameAsync(request.Username);
+        var userEntity = await _dbService.GetUserProfileByUsernameAsync(request.Username);
 
-        // if (userEntity == null)
-        // {
-        //     return NotFound(new { error = $"User '{request.Username}' not found." });
-        // }
+        if (userEntity == null)
+        {
+            return NotFound(new { error = $"User '{request.Username}' not found." });
+        }
 
-        // var response = new GtsUserInfoResponse(
-        //     Username: userEntity.Username,
-        //     DisplayName: userEntity.DisplayName,
-        //     PublicKey: userEntity.PublicKeyPem,
-        //     PrivateKey: userEntity.PrivateKeyPem
-        // );
+        var response = new GtsUserInfoResponse(
+            Username: userEntity.Username,
+            DisplayName: userEntity.Username,
+            PublicKey: userEntity.PublicKeyPem,
+            PrivateKey: userEntity.PrivateKeyPem
+        );
 
-        return Ok();
+        return Ok(response);
     }
 
     [HttpPost("followers")]
     public async Task<IActionResult> GetFollowers([FromBody] GtsCollectionRequest request)
     {
-        // var followerEntities = await _dbService.GetFollowersAsync(request.Username);
-        // var followerUrls = followerEntities
-        //     .Select(entity => entity.FollowerInfo.ActorUrl)
-        //     .ToList();
+        var followerEntities = await _dbService.GetFollowersAsync(request.Username);
+        var followerUrls = followerEntities
+            .Select(entity => entity.FollowerInfo.ActorUrl)
+            .ToList();
 
-        // var response = new GtsCollectionResponse(followerUrls);
-        return Ok();
+        var response = new GtsCollectionResponse(followerUrls);
+        return Ok(response);
     }
 
     [HttpPost("following")]
     public async Task<IActionResult> GetFollowing([FromBody] GtsCollectionRequest request)
     {
-        // var followingEntities = await _dbService.GetFollowingAsync(request.Username);
-        // var followingUrls = followingEntities
-        //     .Select(entity => entity.FollowingInfo.ActorUrl)
-        //     .ToList();
+        var followingEntities = await _dbService.GetFollowingAsync(request.Username);
+        var followingUrls = followingEntities
+            .Select(entity => entity.FollowingInfo.ActorUrl)
+            .ToList();
 
-        // var response = new GtsCollectionResponse(followingUrls);
-        return Ok();
+        var response = new GtsCollectionResponse(followingUrls);
+        return Ok(response);
     }
 }
