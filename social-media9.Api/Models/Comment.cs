f9e098c4a84680275f7d2cf967fe32ff21e54b3e
VuyoSibiya-BBD <vuyo.sibiya@bbd.co.za>
2025-08-07 23:45:53 +0200

    removed unnecesary fields

--- social-media9.Api/Models/Comment.cs
+++ social-media9.Api/Models/Comment.cs
@@ -7,22 +7,13 @@ namespace social_media9.Api.Models
     [DynamoDBTable("nexusphere-mvp-main-table")]
     public class Comment : BaseEntity
     {
-        // [DynamoDBHashKey] // Partition key
-        // public string PostId { get; set; }
-
-        // [DynamoDBRangeKey] // Sort key
-        // public string CommentId { get; set; } = Guid.NewGuid().ToString();
-
-        // public string UserId { get; set; }
-        // public string Username { get; set; } = string.Empty;
-        // public string Text { get; set; } = string.Empty;
-        // public DateTime CreatedAt { get; set; }
+        public string UserId { get; set; }
 
         [DynamoDBProperty("AuthorUsername ")]
         public string Username { get; set; } = string.Empty;
 
         [DynamoDBProperty("Content")]
-        public string Content { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
 
         [DynamoDBProperty("CreatedAt")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
