9172afabacf54156ed9816cd4903ed5ce023f543
VuyoSibiya-BBD <vuyo.sibiya@bbd.co.za>
2025-08-11 04:30:21 +0200

    added unimplemented methods

--- social-media9.Api/Repositories/Implementations/NeptuneFollowRepository.cs
+++ social-media9.Api/Repositories/Implementations/NeptuneFollowRepository.cs
@@ -1,8 +1,31 @@
 
+using social_media9.Api.Models;
 using social_media9.Api.Repositories.Interfaces;
 
 public class NeptuneFollowRepository : IFollowRepository
 {
-  
+    public Task FollowAsync(string followerId, string followingId)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<IEnumerable<Follow>> GetFollowersAsync(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Follow>> GetFollowingAsync(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsFollowingAsync(string followerId, string followingId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UnfollowAsync(string followerId, string followingId)
+    {
+        throw new NotImplementedException();
+    }
 }
