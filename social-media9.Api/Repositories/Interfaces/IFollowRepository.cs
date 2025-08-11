89d020ae5d0aa1234b1a6ae53c7b784b15144c62
VuyoSibiya-BBD <vuyo.sibiya@bbd.co.za>
2025-08-11 04:18:23 +0200

    added additional methods in followrepo

--- social-media9.Api/Repositories/Interfaces/IFollowRepository.cs
+++ social-media9.Api/Repositories/Interfaces/IFollowRepository.cs
@@ -12,24 +12,13 @@ namespace social_media9.Api.Repositories.Interfaces
     /// </summary>
     public interface IFollowRepository
     {
-
-        /// <summary>
-        /// Asynchronously gets a list of user IDs for all followers of a specific user.
-        /// </summary>
-        /// <param name="userId">The ID of the user whose followers are being retrieved.</param>
-        /// <returns>A task that returns an enumerable collection of follower IDs.</returns>
+        Task FollowAsync(string followerId, string followingId);
+        Task<bool> UnfollowAsync(string followerId, string followingId);
         Task<IEnumerable<Follow>> GetFollowersAsync(string userId);
-
-        /// <summary>
-        /// Asynchronously gets a list of user IDs for all users a specific user is following.
-        /// </summary>
-        /// <param name="userId">The ID of the user whose following list is being retrieved.</param>
-        /// <returns>A task that returns an enumerable collection of user IDs being followed.</returns>
         Task<IEnumerable<Follow>> GetFollowingAsync(string userId);
-
         Task<bool> IsFollowingAsync(string followerId, string followingId);
-
     }
+
 }
 
 
